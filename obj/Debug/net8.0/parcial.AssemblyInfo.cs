98976503d242c08d1cfa90750cdf538c3bd8d29d
Julio3021 <julio_antaurco@usmp.pe>
2024-10-04 20:47:31 -0500

    funciones listado

--- obj/Debug/net8.0/parcial.AssemblyInfo.cs
+++ obj/Debug/net8.0/parcial.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("parcial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1a4c635dfa38f5e31ad504f2a80a40ce38b467")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13bce1cd61bc0ccdb857953a2da3b136f04d31d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("parcial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("parcial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
