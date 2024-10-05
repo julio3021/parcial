c58912c101848434a3ff6e44060239f811ee4b65
Julio3021 <julio_antaurco@usmp.pe>
2024-10-04 21:39:51 -0500

    actualizado

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using parcial.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container sql.
@@ -33,6 +34,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+///cambios//
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
